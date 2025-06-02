5e1152fef6fccf5c81575ca7ec07ec333295f02c
sania924 <saniayasin924@gmail.com>
2025-06-02 15:42:43 +0500

    mongoose learning

--- Mongoose/index.js
+++ Mongoose/index.js
@@ -7,3 +7,18 @@ async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/test');
 
 }
+// Schema
+const userSchema=new mongoose.Schema({
+name:String,
+Age:Number,
+Email:String
+})
+// model
+const Employee=mongoose.model("Employee",userSchema)
+// insert 1 document 
+const Employee1=new Employee({
+  name:"sania",
+  Age:30,
+  Email:"@gmail.com"
+})
+Employee1.save();
