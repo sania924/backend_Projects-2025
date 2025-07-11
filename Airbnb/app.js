83a5e56b56a88d5af2723e2e199858b6bac0ab9b
sania924 <saniayasin924@gmail.com>
2025-07-11 14:39:09 +0500

    update Airbnb

--- Airbnb/app.js
+++ Airbnb/app.js
@@ -1,7 +1,9 @@
 const express= require('express');
 const app = express();
+app.set('view engine', 'ejs'); // Set EJS as the view engine
+app.set('views', __dirname + '/views'); // Set the views directory
 const mongoose = require('mongoose');
-
+const Listing = require('./models/listing'); 
 // Connect to MongoDB with Mongoose using then
 mongoose.connect('mongodb://localhost:27017/airbnb')
     .then(() => {
@@ -15,6 +17,38 @@ mongoose.connect('mongodb://localhost:27017/airbnb')
 app.get('/', (req, res) => {        
     res.send('Welcome to Airbnb!');
 });
+
+// Listing Routes
+// index route to get all listings
+app.get('/listings', async (req, res) => {
+    try {
+        const allListings = await Listing.find({});
+        res.render('listing/index', { listings: allListings });
+    } catch (err) {
+        console.error('Error fetching listings:', err);
+        res.status(500).send('Internal Server Error');
+    }
+});
+// show route to get a single listing by ID
+app.get('/listings/:id', async (req, res) => {
+  
+        const listing = await Listing.findById(req.params.id);
+      
+        res.render('listing/show', { listing });
+    
+});
+
+
+
+
+
+
+
+
+
+
+
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
